13d1d2c9835663da59fbd2a9cb8775ff9da3bfe9
antsummer <devsuay@gmail.com>
2024-08-24 17:55:53 -0300

    feat: class to send telegram messages

---
+++ android/app/src/main/kotlin/dev/antsummer/tooltelegram/utils/data/TelegramMessageSender.kt
@@ -0,0 +1,41 @@
+package dev.antsummer.tooltelegram.utils.data
+
+import android.content.Context
+
+import dev.antsummer.tooltelegram.utils.network.RequestNetwork
+import dev.antsummer.tooltelegram.utils.network.RequestListener
+
+class TelegramMessageSender(private val context: Context) {
+
+    fun sendMessage(chatId: String, token: String, message: String, callback: SendMessageCallback) {
+        val url = "https://api.telegram.org/bot$token/sendMessage"
+        val headers = HashMap<String, String>().apply {
+            put("Content-Type", "application/x-www-form-urlencoded")
+        }
+
+        val params = HashMap<String, String>().apply {
+            put("chat_id", chatId)
+            put("text", message)
+        }
+        
+        val queryString = params.entries.joinToString("&") { "${it.key}=${it.value}" }
+
+        val requestNetwork = RequestNetwork(context)
+        requestNetwork.headersSet(headers)
+
+        requestNetwork.startRequestNetwork("POST", "$url?$queryString", "TelegramAPI", object : RequestListener {
+            override fun onResponse(tag: String, response: String, responseHeader: HashMap<String, String>) {
+                callback.onSuccess(response)
+            }
+
+            override fun onErrorResponse(tag: String, response: String) {
+                callback.onError(response)
+            }
+        })
+    }
+
+    interface SendMessageCallback {
+        fun onSuccess(response: String)
+        fun onError(error: String)
+    }
+}
