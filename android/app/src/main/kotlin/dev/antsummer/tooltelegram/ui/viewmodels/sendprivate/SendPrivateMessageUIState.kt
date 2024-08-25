d0491a5f721fa92eaeba922c5607c77667d979fc
Ant Summer <devsuay@gmail.com>
2024-08-24 21:57:00 -0300

    feat: improve UI State

--- android/app/src/main/kotlin/dev/antsummer/tooltelegram/ui/viewmodels/sendprivate/SendPrivateMessageUIState.kt
+++ android/app/src/main/kotlin/dev/antsummer/tooltelegram/ui/viewmodels/sendprivate/SendPrivateMessageUIState.kt
@@ -3,14 +3,8 @@ package dev.antsummer.tooltelegram.ui.viewmodels.sendprivate
 import android.content.Context
 
 data class SendPrivateMessageUIState(
-      val chatId: String = "-100",
-      val token: String = "",
-      val message: String = "Messaged by Tool Telegram Remake",
-      val isSuccess: Boolean = false,
-      val onChatIdChange: (String) -> Unit = {},
-      val onTokenChange: (String) -> Unit = {},
-      val onMessageChange: (String) -> Unit = {},
-      val onClickToSend: (String, String, String, Context) -> Unit = { _, _, _, _ -> },
-      val onSendSuccess: (String) -> Unit = {},
-      val onSendError: (String) -> Unit = {}
+    val chatId: String = "-100",
+    val token: String = "",
+    val message: String = "Messaged by Tool Telegram Remake",
+    val isSuccess: Boolean = false
 )
