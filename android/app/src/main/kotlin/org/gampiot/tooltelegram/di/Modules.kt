bc7048c6d57eaef361e26a0fcf4db9f16de4d588
antsummer <devsuay@gmail.com>
2024-08-27 00:35:38 -0300

    feat: DataStore

--- android/app/src/main/kotlin/org/gampiot/tooltelegram/di/Modules.kt
+++ android/app/src/main/kotlin/org/gampiot/tooltelegram/di/Modules.kt
@@ -9,14 +9,14 @@ import org.koin.core.module.dsl.singleOf
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
-import org.gampiot.tooltelegram.ui.viewmodels.preferences.SettingsViewModel
+import org.gampiot.tooltelegram.ui.viewmodels.preferences.d.AppPreferencesViewModel
 import org.gampiot.tooltelegram.repositories.AppPreferencesRepository
 
 const val APP_PREFERENCES = "app_preferences"
 
 val appModule = module {
     singleOf(::AppPreferencesRepository)
-    viewModelOf(::SettingsViewModel)
+    viewModelOf(::AppPreferencesViewModel)
 }
 
 val appPreferencesModule = module { 
