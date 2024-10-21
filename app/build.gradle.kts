d5863dbd7eb666e5914d1725a68e0e0187b994f9
Aquiles Trindade <devsuay@gmail.com>
2024-10-21 20:31:13 -0300

    chore: bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.trindadedev.tooltelegram"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
         
         vectorDrawables { 
             useSupportLibrary = true
